f5b27301df18991f425c6e05da09462381424a67
GuiTadeu <guitadeu.py@gmail.com>
2021-01-16 14:59:33 -0300

    Adicionando nova regra

--- src/main/java/br/com/alura/rh/service/ReajusteService.java
+++ src/main/java/br/com/alura/rh/service/ReajusteService.java
@@ -5,6 +5,8 @@ import br.com.alura.rh.model.Funcionario;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class ReajusteService {
     public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
@@ -12,7 +14,14 @@ public class ReajusteService {
         BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
 
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salário!");
+        }
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        Long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+        if(mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes deve ser de no mínimo 6 meses");
         }
 
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
