71cdca36cd570dd34e24cfffa9cfc9f55ea84e51
GuiTadeu <guitadeu.py@gmail.com>
2021-01-23 16:49:29 -0300

    Liskov Substitution Principle

--- src/main/java/br/com/alura/rh/model/Terceirizado.java
+++ src/main/java/br/com/alura/rh/model/Terceirizado.java
@@ -2,12 +2,45 @@ package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
 
-public class Terceirizado extends Funcionario {
+public class Terceirizado {
 
+    private DadosPessoais dadosPessoais;
     private String empresa;
 
-    public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-        super(nome, cpf, cargo, salario);
+    public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario, String empresa) {
+        this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+        this.empresa = empresa;
     }
 
+    public String getNome() {
+        return this.dadosPessoais.getNome();
+    }
+
+    public void setNome(String nome) {
+        this.dadosPessoais.setNome(nome);
+    }
+
+    public String getCpf() {
+        return this.dadosPessoais.getCpf();
+    }
+
+    public void setCpf(String cpf) {
+        this.dadosPessoais.setCpf(cpf);
+    }
+
+    public Cargo getCargo() {
+        return this.dadosPessoais.getCargo();
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.dadosPessoais.setCargo(cargo);
+    }
+
+    public BigDecimal getSalario() {
+        return this.dadosPessoais.getSalario();
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.dadosPessoais.setSalario(salario);
+    }
 }
